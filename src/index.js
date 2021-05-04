3898cdf0791b75245bf8ef51f0d797a229c69bda
jwy2002 <50856876+jwy2002@users.noreply.github.com>
2021-05-04 13:44:11 +0900

    eject before

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Say';
+import App from './css/SassComponent';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
